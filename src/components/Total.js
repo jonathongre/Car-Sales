fc440aafe5f6a65afed35b7966ed9022119ecc2e
Jonathon Green <ericg9811@gmail.com>
2019-08-14 16:48:21 -0500

    Updated styling

--- src/components/Total.js
+++ src/components/Total.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Total = ({ car, additionalPrice }) => {
   return (
-    <div className="content">
+    <div className="content total">
       <h4 class="subtitle is-2">Total Amount: ${car.price + additionalPrice}</h4>
     </div>
   );
